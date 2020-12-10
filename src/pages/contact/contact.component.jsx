e881bca0c4e4d7e346db81f6827c1ffb656c4ed9
DESKTOP-TBQ6K2I\USER <michael.yen@varmeego.com>
2020-12-10 23:16:56 +0800

    resized imgs at contact page and npm build

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -6,6 +6,7 @@ import Fb_icon from '../../assets/socialMedia-icon/fb_icon.jpg';
 import Ig_icon from '../../assets/socialMedia-icon/ig_icon.jpg';
 import Linkedin_icon from '../../assets/socialMedia-icon/linkedin_icon.jpg';
 import Whatsapp_icon from '../../assets/socialMedia-icon/whatsapp_icon.png';
+import Github_icon from '../../assets/socialMedia-icon/github_icon.webp';
 
 import './contact.styles.scss';
 
@@ -14,9 +15,10 @@ function Contact() {
     let igItem = useRef(null);
     let wtpItem = useRef(null);
     let linkedinItem = useRef(null);
+    let githubItem = useRef(null);
 
     useEffect(() => {
-        TweenMax.staggerFrom([fbItem, igItem, wtpItem, linkedinItem], 1, { opacity: 0, y: 100, ease: Power3.easeOut }, .15)
+        TweenMax.staggerFrom([fbItem, igItem, wtpItem, linkedinItem, githubItem], 1, { opacity: 0, y: 100, ease: Power3.easeOut }, .1)
     }, [])
 
     return (
@@ -35,32 +37,39 @@ function Contact() {
                     </div>
                     <div className="contact-inner-bottom-right">
                         <div className="socialMedia-container">
-                            <div 
-                            ref={el => { fbItem = el }}
-                            className="socialMedia-container-inner">
+                            <div
+                                ref={el => { fbItem = el }}
+                                className="socialMedia-container-inner">
                                 <a href="https://www.facebook.com/michael.yen.71/" target="_blank" rel="noopener noreferrer">
                                     <img src={Fb_icon} alt="Fb_icon" />
                                 </a>
                             </div>
-                            <div 
-                            ref={el => { igItem = el }}
-                            className="socialMedia-container-inner">
+                            <div
+                                ref={el => { igItem = el }}
+                                className="socialMedia-container-inner">
                                 <a href="https://www.instagram.com/michael__yen/" target="_blank" rel="noopener noreferrer">
                                     <img src={Ig_icon} alt="Ig_icon" />
                                 </a>
                             </div>
-                            <div 
-                            ref={el => { wtpItem = el }}
-                            className="socialMedia-container-inner">
+                            <div
+                                ref={el => { wtpItem = el }}
+                                className="socialMedia-container-inner">
                                 <a href="https://wa.me/92005922" target="_blank" rel="noopener noreferrer">
                                     <img src={Whatsapp_icon} alt="Whatsapp_icon" />
                                 </a>
                             </div>
-                            <div 
-                            ref={el => { linkedinItem = el }}
-                            className="socialMedia-container-inner">
+                            <div
+                                ref={el => { linkedinItem = el }}
+                                className="socialMedia-container-inner">
                                 <a href="https://www.linkedin.com/in/yen-michael-3ba255176/" target="_blank" rel="noopener noreferrer">
                                     <img src={Linkedin_icon} alt="Linkedin_icon" />
+                                </a>
+                            </div>
+                            <div
+                                ref={el => { githubItem = el }}
+                                className="socialMedia-container-inner">
+                                <a href="https://github.com/hzyen" target="_blank" rel="noopener noreferrer">
+                                    <img src={Github_icon} alt="Github_icon" />
                                 </a>
                             </div>
                         </div>
