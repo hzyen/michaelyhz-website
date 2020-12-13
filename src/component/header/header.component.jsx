5e2852a4fbba748606c1f0f4b6e9244a935045d6
hzyen <a03036yen@yahoo.com.hk>
2020-12-14 03:43:04 +0800

    added responsive menu bar and menu button

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { TweenMax, Power3 } from 'gsap';
 import { Link } from 'react-router-dom';
 
 import Logo from '../../assets/michaelyhz_logo.png';
@@ -6,68 +7,98 @@ import MenuItem from '../menuItem/menuItem.component';
 
 import './header.styles.scss';
 
-class Header extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            menuBarData: {
-                "menuBar": [
-                    {
-                        "id": 1,
-                        "name": "Home",
-                        "linkUrl": "/",
-                    },
-                    {
-                        "id": 2,
-                        "name": "About",
-                        "linkUrl": "/about",
-                    },
-                    {
-                        "id": 3,
-                        "name": "Project",
-                        "linkUrl": "/project",
-                    },
-                    {
-                        "id": 4,
-                        "name": "Album",
-                        "linkUrl": "/album",
-                    },
-                    {
-                        "id": 5,
-                        "name": "Contact",
-                        "linkUrl": "/contact",
-                    }
-                ]
-            },
+const menuBarData = {
+    "menuBar": [
+        {
+            "id": 1,
+            "name": "Home",
+            "linkUrl": "/",
+        },
+        {
+            "id": 2,
+            "name": "About",
+            "linkUrl": "/about",
+        },
+        {
+            "id": 3,
+            "name": "Project",
+            "linkUrl": "/project",
+        },
+        {
+            "id": 4,
+            "name": "Album",
+            "linkUrl": "/album",
+        },
+        {
+            "id": 5,
+            "name": "Contact",
+            "linkUrl": "/contact",
         }
+    ]
+}
+
+const Header = () => {
+    const [mobile_menu_button_className, set_mobile_menu_className] = useState("mobile-menu-button");
+    const [mobile_menu_button, toggle_mobile_menu] = useState(false);
+    let menuButtonItem = useRef(null);
+
+    const mobileMenuExpand = () => {
+        set_mobile_menu_className("mobile-menu-button-active")
+        TweenMax.to(menuButtonItem, .2, { opacity: 0, y: -150, ease: Power3.easeIn, css: {opacity: 1} });
+        toggle_mobile_menu(true);
+        console.log(mobile_menu_button)
     }
 
-    render() {
-        const { menuBarData } = this.state;
-        return (
-            <div className="header-container">
-                <div className="header-container-inner mainPage-styles">
-                    <div className="header-container-left">
-                        <div className="menu-icon">
-                            <Link to="/"><p id="name-wrap"><span id="name">MY</span></p></Link>
-                            {/* <Link to="/"><p id="name-wrap"><img src={Logo} /></p></Link> */}
-                        </div>
-                    </div>
-                    <div className="header-container-right">
-                        <div className="menu">
-                            {
-                                menuBarData.menuBar.map(
-                                    ({ id, ...othersProps }) =>
-                                        (<MenuItem key={id} {...othersProps} />)
-                                )
-                            }
-                        </div>
+    const mobileMenuShirk = () => {
+        set_mobile_menu_className("mobile-menu-button")
+        TweenMax.to(menuButtonItem, .2, { opacity: 1, y: -150, ease: Power3.easeOut, css: {opacity: 0} });
+        toggle_mobile_menu(false);
+        console.log(mobile_menu_button)
+    }
+
+    return (
+        <div className="header-container">
+            <div
+                ref={el => menuButtonItem = el}
+                className="mobile-menu">
+                <div className="mobile-menu-container">
+                    <div className="mobile-menu-container-inner">
+                        {
+                            menuBarData.menuBar.map(
+                                ({ id, ...othersProps }) =>
+                                    (<MenuItem key={id} {...othersProps} />)
+                            )
+                        }
                     </div>
                 </div>
             </div>
-        );
-    }
+            <div className="header-container-inner mainPage-styles">
+                <div className="header-container-left">
+                    <div className="menu-icon">
+                        <Link to="/"><p id="name-wrap"><span id="name">MY</span></p></Link>
+                        {/* <Link to="/"><p id="name-wrap"><img src={Logo} /></p></Link> */}
+                    </div>
+                </div>
+                <div className="header-container-right">
+                    <div className="menu">
+                        {
+                            menuBarData.menuBar.map(
+                                ({ id, ...othersProps }) =>
+                                    (<MenuItem key={id} {...othersProps} />)
+                            )
+                        }
+                    </div>
+                    <div
+                        onClick={mobile_menu_button !== true ? mobileMenuExpand : mobileMenuShirk}
+                        className={mobile_menu_button_className}>
+                        <div className="blackline-1"></div>
+                        <div className="blackline-2"></div>
+                        <div className="blackline-3"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
--- src/component/menuItem/menuItem.component.jsx
+++ src/component/menuItem/menuItem.component.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 import './menuItem.styles.scss';
 
 const MenuItem = ({ name, linkUrl }) => (
-    <div className='menu-item-container'>
-        <div className="menu-item-inner">
-            <Link className="menu-item" to={linkUrl}><p>{name}</p></Link>
+    <div className='menu-item-container mobile-menu-item-container'>
+        <div className="menu-item-inner mobile-menu-item-inner">
+            <Link className="menu-item mobile-menu-item" to={linkUrl}><p>{name}</p></Link>
         </div>
     </div>
 )
